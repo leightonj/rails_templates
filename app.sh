32bc3886f92d3e0c3f759fecb66962e2b704f87c
Leighton James <leighton0102@gmail.com>
2024-08-23 20:43:36 +0100

    create app shell script

---
+++ app.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+
+rm -rf blog
+rails new blog --template=rails_templates/standard.rb --database=postgresql --skip-test --skip-javascript --skip-hotwire --skip-bundle --quiet
+# rails new blog --template=https://raw.githubusercontent.com/leightonj/rails_templates/main/standard.rb --database=postgresql --skip-test --quiet
+
+code blog
