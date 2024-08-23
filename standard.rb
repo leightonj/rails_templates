f66c16ddeb865374255f5a304f68991335059936
Leighton James <leighton0102@gmail.com>
2024-08-23 16:37:16 +0100

    rc stuff

--- standard.rb
+++ standard.rb
@@ -1,9 +1,13 @@
 after_bundle do
   generate("rspec:install")
-  # rails generate rspec:install
+
   # git :init
   # git add: "."
   # git commit: %Q{ -m 'Initial commit' }
+
+  # file ".rubocop.yml", open("../rails_templates/.rubocop.yml") { |f| f.read }
+  file ".rubocop.yml", URI.open("https://raw.githubusercontent.com/leightonj/rails_templates/main/.rubocop.yml") { |f| f.read }
+  run("rubocop -A")
 end
 
 gem_group :development, :test do
