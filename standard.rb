fac2dc1033fb054f4369295902d875d8e1674dce
Leighton James <leighton0102@gmail.com>
2024-08-23 16:44:25 +0100

    rspc rails

--- standard.rb
+++ standard.rb
@@ -5,12 +5,14 @@ after_bundle do
   # git add: "."
   # git commit: %Q{ -m 'Initial commit' }
 
-  # file ".rubocop.yml", open("../rails_templates/.rubocop.yml") { |f| f.read }
-  file ".rubocop.yml", URI.open("https://raw.githubusercontent.com/leightonj/rails_templates/main/.rubocop.yml") { |f| f.read }
+  file ".rubocop.yml", open("../rails_templates/.rubocop.yml") { |f| f.read }
+  # file ".rubocop.yml", URI.open("https://raw.githubusercontent.com/leightonj/rails_templates/main/.rubocop.yml") { |f| f.read }
   run("rubocop -A")
 end
 
 gem_group :development, :test do
   gem "rspec-rails"
   gem "rubocop-rails"
+  gem "rubocop-rspec", require: false
+  gem "rubocop-rspec_rails", require: false
 end
