6f8a9d25738be615a3c9e056ebcf5f32319d350a
Leighton James <leighton0102@gmail.com>
2024-08-23 20:41:16 +0100

    devise

--- standard.rb
+++ standard.rb
@@ -1,27 +1,91 @@
 # frozen_string_literal: true
 
-def rubocop
-  # file ".rubocop.yml", open("../rails_templates/.rubocop.yml") { |f| f.read }
-  file ".rubocop.yml", URI.open("https://raw.githubusercontent.com/leightonj/rails_templates/main/.rubocop.yml", &:read)
+def setup_rubocop
+  file ".rubocop.yml", open("../rails_templates/.rubocop.yml", &:read)
+  # file ".rubocop.yml", URI.open("https://raw.githubusercontent.com/leightonj/rails_templates/main/.rubocop.yml", &:read)
   git add: ".rubocop.yml"
-  git commit: "-am 'rubocop'"
+  git commit: "-m 'rubocop'"
+end
+
+def setup_rspec
+  generate("rspec:install")
+  git add: ".rspec spec"
+  git commit: "-m 'rspec'"
+end
+
+def setup_simple_form
+  generate("simple_form:install")
+  git add: "config/initializers/simple_form.rb config/locales/simple_form.en.yml lib/templates"
+  git commit: "-m 'simple_form'"
+end
+
+def setup_user
+  generate(:scaffold, "user", "first_name:string{50}", "last_name:string{50}", "deleted_at:datetime")
+  rails_command "db:migrate"
+  git add: "."
+  git commit: "-am 'User scaffold'"
+end
+
+def setup_cancancan
+  generate("cancan:ability")
+  git add: "app/models/ability.rb"
+  git commit: "-m 'cancancan'"
+end
+
+def setup_delayed_job
+  generate("delayed_job:active_record")
+  git add: "bin/delayed_job db/migrate db/schema.rb"
+  git commit: "-m 'active_record'"
+  rails_command "db:migrate"
+end
+
+def setup_whenever
+  run("wheneverize")
+  git add: "config/schedule.rb"
+  git commit: "-m 'whenever'"
+end
+
+def setup_devise
+  generate("devise:install")
+  generate("devise:views")
+  generate(:devise, "User")
+  rails_command "db:migrate"
+  git add: "."
+  git commit: "-m 'devise'"
 end
 
 after_bundle do
-  generate("rspec:install")
-  generate("simple_form:install")
+  rails_command "db:drop"
+  rails_command "db:create"
 
-  git :init
+  # git :init
   git add: "."
-  git commit: "-a -m 'Initial commit'"
-  rubocop
+  git commit: "-am 'Initial commit'"
+
+  setup_rspec
+  setup_simple_form
+  setup_user
+  setup_cancancan
+  setup_delayed_job
+  setup_whenever
+  setup_devise
+  setup_rubocop
+
   # pp run("rubocop -A")
 end
 
 gem "aasm"
+gem "cancancan"
+# gem "dalli"
+gem "delayed_job_active_record"
+gem "devise"
 gem "faker"
+gem "faraday"
+# gem "jb"
+# gem "rollbar"
 gem "simple_form"
 gem "timecop"
+gem "whenever", require: false
 
 gem_group :development, :test do
   gem "factory_bot_rails"
@@ -36,3 +100,10 @@ end
 # RSpec.configure do |config|
 #   config.include FactoryBot::Syntax::Methods
 # end
+
+# config/environments/development.rb
+# config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+# app/views/layouts/application.html.erb
+# <p class="notice"><%= notice %></p>
+# <p class="alert"><%= alert %></p>
